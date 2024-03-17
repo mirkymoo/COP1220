b3607a51606d1feb3626fc4ab2e6ecb6f0d67d4e
mirkymoo <vlada.cherkasskaya@gmail.com>
2024-03-17 19:45:51 -0400

    changed switch statement to else

--- random_number_game.c
+++ random_number_game.c
@@ -7,7 +7,7 @@ int main() {
     int user_guess, computer_choice, difference;
 
     //get user input
-    printf("Guess a number between 1-5. Please enter your choice:");
+    printf("Guess a number between 1-5.\nPlease enter your choice: ");
     scanf("%d", &user_guess);
 
     //random number generator
@@ -16,25 +16,15 @@ int main() {
 
     // Determine the winner
     if (user_guess == computer_choice) {
-        printf("The difference between your guess and the computers choice is 0! Congradulations, you guessed the number.\n");
+        printf("Does %d equal %d? %s\n", computer_choice, user_guess, user_guess == computer_choice ? "true" : "false");
+        printf("The difference between your guess and the random number is 0! Congradulations, you guessed the number.\n");
     } 
-    else if (user_guess != computer_choice) {
-        switch(computer_choice) {
-        case user_guess > computer_choice:
-            difference = user_guess - computer_choice;
-            printf("The difference between the random number and your guess is %d\n", difference);
-            printf("The correct answer was %d\n", computer_choice);
-            printf("Does %d equal %d?\n", computer_choice, user_guess);
-            printf("%s\n", user_guess == computer_choice ? "true" : "false");
-            break;
-        case user_guess < computer_choice:
-            difference = computer_choice - user_guess;
-            printf("The difference between the random number and your guess is %d\n", difference);
-            printf("The correct answer was %d\n", computer_choice);
-            printf("Does %d equal %d?\n", computer_choice, user_guess);
-            printf("%s\n", user_guess == computer_choice ? "true" : "false");
-            break;
-        }
+    else {
+        // Calculate the difference
+        difference = abs(computer_choice - user_guess);
+        printf("Does %d equal %d? %s\n", computer_choice, user_guess, user_guess == computer_choice ? "true" : "false");
+        printf("The difference between the random number and your guess is %d.\n", difference);
+        printf("The correct answer was %d.\n", computer_choice);
     }
     
     return 0;
